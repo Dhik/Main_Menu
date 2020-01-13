539d1ed9c772cce9c4d4164ba2f58a9d276e8c8f
Rashomon <mohammad18018@mail.unpad.ac.id>
2020-01-13 10:39:22 +0700

    Main Menu

---
+++ Assets/Bahan MainMenu/Menu_Manager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu_Manager : MonoBehaviour
+{
+    public GameObject MenuPanel;
+    public GameObject CreditsPanel;
+    public GameObject HowtoPanel;
+    public GameObject MenPanel;
+    public GameObject CreditPanel;
+    public GameObject HowtPanel;
+    public GameObject MePanel;
+    public GameObject CrediPanel;
+    public GameObject HowPanel;
+    void Start()
+    {
+        MenuPanel.SetActive(true);
+        CreditsPanel.SetActive(false);
+        HowtoPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void PlayButtonClicked()
+    {
+        Application.LoadLevel("AR Game");
+    }
+    public void CreditsButtonClicked()
+    {
+        MenuPanel.SetActive(false);
+        CreditsPanel.SetActive(true);
+        HowtoPanel.SetActive(false);
+    }
+    public void HowtoButtonClicked()
+    {
+        MenuPanel.SetActive(false);
+        CreditsPanel.SetActive(false);
+        HowtoPanel.SetActive(true);
+    }
+    public void Quit_Clicked()
+    {
+        Application.Quit();
+    }
+    public void BackButtonClicked()
+    {
+        MenuPanel.SetActive(true);
+        CreditsPanel.SetActive(false);
+        HowtoPanel.SetActive(false);
+    }
+}
---
+++ Assets/Bahan MainMenu/NewBehaviourScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject MainPanel;
+    public GameObject MenuPanel;
+    public GameObject CreditsPanel;
+    public GameObject HowtoPanel;
+    public GameObject MenPanel;
+    public GameObject CreditPanel;
+    public GameObject HowtPanel;
+    public GameObject MePanel;
+    public GameObject CrediPanel;
+    public GameObject HowPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
